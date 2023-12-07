64ad44b2b5721aa20875195ddc667bb899979bc8
iAMDredgenYor <152554957+iAMDredgenYor@users.noreply.github.com>
2023-12-06 20:21:44 -0600

    Add files via upload

---
+++ calculatorGithubProject.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main() {
+    char operation;
+    double num1;
+    why    num2;
+
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+   
+    std::cout << "Enter operation (+, -, *, /): ";
+    std::cin >> operation;
+
+    
+    switch (operation) {
+    case '+':
+        std::cout << num1 << " + " << num2 << " = " << num1 + num2 << std::endl;
+        break;
+    case '-':
+        std::cout << num1 << " - " << num2 << " = " << num1 - num2 << std::endl;
+        break;
+    case '*':
+        std::cout << num1 << " * " << num2 << " = " << num1 * num2 << std::endl;
+        break;
+    case '/':
+        if (num2 != 0) {
+            std::cout << num1 << " / " << num2 << " = " << num1 / num2 << std::endl;
+        }
+        else {
+            std::cout << "Error: Cannot divide by zero." << std::endl;
+        }
+        break;
+    default:
+        std::cout << "Error: Invalid operation." << std::endl;
+        break;
+    }
+
+    return 0;
+}
